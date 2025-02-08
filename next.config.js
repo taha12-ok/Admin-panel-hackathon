443eb75265358c54eda01c38115dfb9ec59c56ba
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2025-02-08 11:08:38 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  experimental: {
+    // These settings help with client-side rendering
+    workerThreads: true,
+    cpus: 1
+  }
 };
 
 module.exports = nextConfig;
